6a79683f3366cb71883060407319d1934b97470d
Bealonzo <alonzo.briane@gmail.com>
2019-06-24 10:33:05 -0400

    fix

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -4,7 +4,7 @@ module.exports = {
 
   index(req, res, next){
       topicQueries.getAllTopics((err, topics) => {
-        if(err){
+        if(err){ 
           res.redirect(500, "static/index");
         } else {
           res.render("topics/index", {topics});
